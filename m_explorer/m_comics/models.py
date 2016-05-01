0e18e77a60089b50d45518bc60c37729b0822843
paulsheridan <paul.sheridan@me.com>
2016-05-01 16:06:49 -0700

    added Comic, ReadingList and User models, still unfinished but a good start. no serializers or URLs yet.

---
+++ m_explorer/m_comics/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.conf import settings
+
+
+@python_2_unicode_compatible
+class ReadingList(models.Model):
+    """
+    The model for a user's list of comics to be read.
+    """
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='readinglist')
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    comics = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='lists')
+
+    def __str__(self):
+        return self.title
+
+
+@python_2_unicode_compatible
+class Comic(models.Model):
+    """
+    Comic model for individual comics saved
+    to the user's coimc list model
+    """
+    marvel_id = models.IntigerField()
+    title = models.CharField()
+    issue_number = models.IntigerField()
+    description = models.CharField()
+    isbn = models.CharField()
+    page_count = models.IntigerField()
+    url = models.URLField()
+    series = models.IntigerField()
+    purchase = models.URLField()
+    read = models.BooleanField()
+
+    def __str__(self):
+        return self.title
---
+++ m_explorer/m_profile/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.conf import settings
+from .m_comics.models import Comic
+
+
+class ActiveUserManager(models.Manager):
+    def get_queryset(self):
+        qs = super(ActiveUserManager, self).get_queryset()
+        return qs.filter(user__is_active__exact=True)
+
+
+@python_2_unicode_compatible
+class MarvelProfile(models.Model):
+    '''
+    User profile model attached to Django's
+    built-in User model
+    '''
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='profile')
+    location = models.CharField(
+        default='',
+        max_length=255)
+    bio = models.TextField(default='')
+    comics = models.ManyToManyField(Comic)
+
+    objects = models.Manager()
+    active = ActiveUserManager()
+
+    def __str__(self):
+        return self.user
+
+    @property
+    def is_active(self):
+        """
+        Return True if target User
+        is active, return False if inactive.
+        """
+        return self.user.is_active
+
+    # def follow(self, target):
+    #     profile = MarvelProfile.objects.get(
+    #         user__username__exact=target)
+    #     self.following.add(profile)
+
+    # def unfollow(self, target):
+    #     profile = MarvelProfile.objects.get(
+    #         user__username__exact=target)
+    #     self.following.remove(profile)
