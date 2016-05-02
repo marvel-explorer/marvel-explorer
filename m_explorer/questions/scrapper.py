b77ec96435c7480c24d5f0dcac92a97b6b5c82b7
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-05-01 19:31:30 -0700

    Got main p tags out.  Still needs work.

---
+++ m_explorer/questions/scrapper.py
@@ -0,0 +1,54 @@
+"""Scrapper to produce character information."""
+from bs4 import BeautifulSoup
+import requests
+import io
+import re
+import sys
+
+MARVEL_UNIVERSE_DOMAIN = 'http://marvel.com/universe'
+CHARACTER = '/Black_Widow_(Natasha_Romanova)'
+
+
+def get_page():
+    "Return content and encoding of desired page."
+    url = MARVEL_UNIVERSE_DOMAIN + CHARACTER
+    resp = requests.get(url)
+    resp.raise_for_status
+    return resp.content, resp.encoding
+
+
+def write_file(html, name):
+    "Write the response html to file"
+    file = open(name, "w")
+    file.write(html)
+    file.close()
+
+
+def load_page(name):
+    """Return html page from file."""
+    content, encoding = get_page()
+    write_file(content.decode(encoding), name)
+    file = io.open(name, "r")
+    return file
+
+
+def parse_source(html):
+    """Return parsed html."""
+    parsed = BeautifulSoup(html, 'html5lib')
+    return parsed
+
+
+def extract_marvel_u_data(html):
+    """Extract div of correct id from marvl u."""
+    div_finder = html.find('div', id='powerbox')  # May need to invoke alternate search
+    return div_finder
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        html = load_page('page.html')
+    else:
+        html = get_page()
+    doc = parse_source(html)
+    doc = extract_marvel_u_data(doc)
+    print(doc.find_all('p'))
