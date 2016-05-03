b51ded2a621979938241a48cdcf4cc512508275f
paulsheridan <paul.sheridan@me.com>
2016-05-03 15:29:01 -0700

    more views wired, new view-specific authentication, password isnt being hashed on save, looking in to the possibility of a third party auth framework.

--- m_explorer/m_profile/handlers.py
+++ m_explorer/m_profile/handlers.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 from m_profile.models import MarvelProfile
+from m_comics.models import ReadingList
 from rest_framework.authtoken.models import Token
 import logging
 
@@ -12,7 +13,6 @@ logger = logging.getLogger(__name__)
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, **kwargs):
-    import pdb; pdb.set_trace()
     if kwargs.get('created', False):
         instance = kwargs.get('instance', None)
         if instance is not None:
@@ -25,13 +25,16 @@ def create_user_profile(sender, **kwargs):
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
+def create_auth_token(sender, **kwargs):
+    instance = kwargs.get('instance', None)
+    created = kwargs.get('created', None)
     if created:
         Token.objects.create(user=instance)
 
 
 @receiver(pre_delete, sender=settings.AUTH_USER_MODEL)
-def remove_user_profile(sender, instance=None, created=False, **kwargs):
+def remove_user_profile(sender, **kwargs):
+    instance = kwargs.get('instance', None)
     try:
         instance.profile.delete()
     except (KeyError, AttributeError):
--- m_explorer/m_profile/views.py
+++ m_explorer/m_profile/views.py
@@ -1,21 +1,37 @@
 from django.contrib.auth.models import User
-from m_profile.serializers import UserSerializer
+from m_profile.models import MarvelProfile
+from m_comics.models import Comic
+from m_profile.serializers import UserSerializer, ProfileSerializer
+from m_comics.serializers import ComicSerializer
 from rest_framework import authentication
 from rest_framework import permissions
 from rest_framework import generics
 
 
-class UserList(generics.ListCreateAPIView):
-    # authentication_classes = (authentication.TokenAuthentication,)
-    # permission_classes = (permissions.IsAdminUser,)
+class CreateUser(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UpdateUser(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    # authentication_classes = (authentication.TokenAuthentication,)
-    # permission_classes = (permissions.IsAdminUser,)
+class UpdateProfile(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
 
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    queryset = MarvelProfile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class UserComics(generics.ListCreateAPIView):
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    queryset = Comic.objects.all()
+    serializer_class = ComicSerializer
