bda64937e76fe581bacf0430b5b1ac479fe31624
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-05-03 11:02:30 -0700

    Character Model adjustment.

--- m_explorer/questions/models.py
+++ m_explorer/questions/models.py
@@ -16,39 +16,38 @@ class CharacterManager(models.Manager):
 @python_2_unicode_compatible
 class Character(models.Model):
     """Create Character Stats model for the database."""
-    name = models.CharField(max_length=60) # got it
-    real_name = models.CharField(max_length=100, blank=True) # got it
-    marvel_name = models.CharField(max_length=100) # api
+    name = models.CharField(max_length=60, null=False, blank=True) # got it
+    real_name = models.CharField(max_length=100, null=False, blank=True) # got it
+    marvel_name = models.CharField(max_length=100, null=False, blank=True) # api
     marvel_id = models.CharField(max_length=20, primary_key=True) # api
-    description = models.CharField(max_length=250) # api
-    thumbnail = models.CharField(max_length=250) # api
-    gender = models.CharField(max_length=30) # create counting function
-    pob = models.CharField(max_length=200, blank=True) # got it
+    description = models.CharField(max_length=250, null=False, blank=True) # api
+    thumbnail = models.CharField(max_length=250, null=False, blank=True) # api
+    gender = models.CharField(max_length=30, null=False, blank=True) # create counting function
+    pob = models.CharField(max_length=200, null=False, blank=True) # got it
     # species = models.CharField(max_length=60, blank=True) # wikipedia stretch
     # race = models.CharField(max_length=70, blank=True) # wikipedia stretch
-    citizenship = models.CharField(max_length=90, blank=True) # I have
-    occupation = models.CharField(max_length=200, blank=True) # I have
-    powers = models.CharFild(max_length=200, blank=True) # wikipedia stretch
-    comics = models.CharField(max_length=250)
+    citizenship = models.CharField(max_length=90, null=False, blank=True) # I have
+    occupation = models.CharField(max_length=200, null=False, blank=True) # I have
+    powers = models.CharFild(max_length=200, null=False, blank=True) # wikipedia stretch
     total_comics = models.IntegerField()  # api
     golden = models.NullBooleanField() # api analysis
     silver = models.NullBooleanField() # api analysis
     bronze = models.NullBooleanField() # api analysis
     dark = models.NullBooleanField() # api analysis
     modern = models.NullBooleanField() # api analysis
-    hair = models.CharField(max_length=30)
-    eyes = models.CharField(max_length=30)
-    first_appearance = models.CharField(max_length=60)
-    identity_status = models.CharField(max_length=60)
-    aliases = models.CharField(max_length=250)
-    weight = models.CharField(max_length=60)
-    height = models.CharField(max_length=60)
-    group_aff = models.CharField(max_length=250)
-    paraphernalia = models.CharField(max_length=250)
-    education = models.CharField(max_length=60)
-    occupation = models.CharField(max_length=250)
-    abilities = models.CharField(max_length=250)
-    weapons = models.CharField(max_length=250)
-    origin = models.CharField(max_length=250)
+    hair = models.CharField(max_length=30, null=False, blank=True)
+    eyes = models.CharField(max_length=30, null=False, blank=True)
+    first_appearance = models.CharField(max_length=60, null=False, blank=True)
+    identity_status = models.CharField(max_length=60, null=False, blank=True)
+    aliases = models.CharField(max_length=250, null=False, blank=True)
+    weight = models.CharField(max_length=60, null=False, blank=True)
+    height = models.CharField(max_length=60, null=False, blank=True)
+    group_aff = models.CharField(max_length=250, null=False, blank=True)
+    paraphernalia = models.CharField(max_length=250, null=False, blank=True)
+    education = models.CharField(max_length=60, null=False, blank=True)
+    occupation = models.CharField(max_length=250, null=False, blank=True)
+    abilities = models.CharField(max_length=250, null=False, blank=True)
+    weapons = models.CharField(max_length=250, null=False, blank=True)
+    origin = models.CharField(max_length=250, null=False, blank=True)
 
     objects = CharacterManager()
