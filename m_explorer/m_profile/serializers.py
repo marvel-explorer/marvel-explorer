6c7650de9e0aacb0e9b98b4465018fc40fe18678
paulsheridan <paul.sheridan@me.com>
2016-05-03 17:17:32 -0700

    serializers updated to hash password and urls changed slightly.

--- m_explorer/m_profile/serializers.py
+++ m_explorer/m_profile/serializers.py
@@ -8,6 +8,12 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'password', 'email')
 
+    def create(self, validated_data):
+        user = User(email=validated_data['email'], username=validated_data['username'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
--- m_explorer/m_profile/urls.py
+++ m_explorer/m_profile/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import CreateUser, UpdateUser, UserComics
 
 urlpatterns = [
-    url(r'^new$', CreateUser.as_view()),
+    url(r'^/new$', CreateUser.as_view()),
     url(r'^(?P<pk>[0-9]+)$', UpdateUser.as_view()),
     url(r'^(?P<pk>[0-9]+)/comics$', UserComics.as_view()),
 ]
