9d6236a623da040a23c164e1d9695f82bbb8d628
paulsheridan <paul.sheridan@me.com>
2016-05-04 14:09:52 -0700

    new comic viewset, no routes for it yet so they are commented out.

--- m_explorer/m_comics/views.py
+++ m_explorer/m_comics/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from .serializers import ComicSerializer, ReadingListSerializer
+from rest_framework import permissions
+from m_profile.permissions import IsObjectOwner
+from rest_framework import authentication
+from rest_framework import viewsets
 
-# Create your views here.
+
+class ComicViewSet(viewsets.ModelViewSet):
+    """
+    Comprehensive viewset for Comics, providing list, create, retrieve,
+    update and destroy actions.
+    """
+    serializer_class = ComicSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated, IsObjectOwner)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        return self.request.user.comics.all()
