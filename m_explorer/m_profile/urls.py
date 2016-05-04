836d42923df2195b630dbb5a74273cb2e1064968
paulsheridan <paul.sheridan@me.com>
2016-05-04 09:12:24 -0700

    further url modifications for the sake of simplicity

--- m_explorer/m_profile/urls.py
+++ m_explorer/m_profile/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from rest_framework.authtoken import views
 from .views import CreateUser, UpdateUser, UserComics
 
 urlpatterns = [
     url(r'^new$', CreateUser.as_view()),
+    url(r'^signin$', views.obtain_auth_token),
     url(r'^(?P<pk>[0-9]+)$', UpdateUser.as_view()),
     url(r'^(?P<pk>[0-9]+)/comics$', UserComics.as_view()),
 ]
