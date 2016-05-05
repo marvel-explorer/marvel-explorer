29c8c12165e34294fb42f557e5ad3145ff6f5208
paulsheridan <paul.sheridan@me.com>
2016-05-05 08:54:33 -0700

    small handful of tests, learning factory boy and requirements updated

--- m_explorer/m_profile/tests.py
+++ m_explorer/m_profile/tests.py
@@ -1,19 +1,53 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from .models import MarvelProfile
+from m_comics.models import Comic, ReadingList
+import factory
 
 
-class TestMarvelProfile(TestCase):
-    """Test profile and user models"""
-    def setUpd(self):
-        self.test_user1 = User.objects.create_user(username='testuser',
-                                                   email='test@test.com',
-                                                   password='testpassword')
-        self.test_user2 = User.objects.create_user(username='testuser2',
-                                                   email='test2@test.com',
-                                                   password='testpassword')
-        self.test_user1.save()
-        self.test_user2.save()
+class UserFactory(factory.django.DjangoModelFactory):
+    """User factory model"""
+    class Meta:
+        model = User
 
-    def test_user_exists(self):
-        """Verify user has been created."""
-        self.assertEquals(len(User.objects.all()), 2)
+
+class ComicFactory(factory.django.DjangoModelFactory):
+    """Comic factory model"""
+    class Meta:
+        model = Comic
+
+
+class NewUserTestCase(TestCase):
+    def setUp(self):
+        self.beast = UserFactory.create(
+            username='beast',
+            email='beast@xmen.com'
+            )
+        self.beast.set_password('xavier')
+        self.cyclops = UserFactory.create(
+            username='cyclops',
+            email='cyclops@xmen.com'
+            )
+        self.cyclops.set_password('xavier')
+        self.rogue = UserFactory.create(
+            username='rogue',
+            email='rogue@xmen.com'
+            )
+        self.rogue.set_password('xavier')
+
+    def test_users_exist(self):
+        self.assertTrue(self.beast)
+        self.assertTrue(self.cyclops)
+        self.assertTrue(self.rogue)
+
+    def test_users_have_profile(self):
+        self.assertTrue(self.beast.profile)
+        self.assertTrue(self.cyclops.profile)
+        self.assertTrue(self.rogue.profile)
+
+    def test_user_has_readinglist(self):
+        self.assertTrue(self.beast.readinglist)
+        self.assertTrue(self.cyclops.readinglist)
+        self.assertTrue(self.rogue.readinglist)
+
+    def test_readinglist_has_comics(self):
