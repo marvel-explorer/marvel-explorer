edd6243dd9f803dea5d18dc0ed7c132da3ec0963
paulsheridan <paul.sheridan@me.com>
2016-05-05 20:27:40 -0700

    model and view tests written and largely passing

--- m_explorer/questions/tests_scrapper.py
+++ m_explorer/questions/tests_scrapper.py
@@ -1,10 +1,10 @@
-# -*- coding:utf-8 -*-
-"""Test scrapper module."""
-import unittest
-
-
-class ScrapperTests(unittest.TestCase):
-    """Basic Test for Scrapper module."""
-
-    def test(self):
-        
+# # -*- coding:utf-8 -*-
+# """Test scrapper module."""
+# import unittest
+#
+#
+# class ScrapperTests(unittest.TestCase):
+#     """Basic Test for Scrapper module."""
+#
+#     def test(self):
+#
